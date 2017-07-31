7245d99df612cd0945620f984358d54782f345dc
barceja2 <barceja2@ZHA-ITA028-W7.corp.oocl.com>
2017-07-31 15:33:15 +0800

    first commit;

---
+++ src/day1GitTraining/gitTraining1.java
@@ -0,0 +1,10 @@
+package day1GitTraining;
+
+public class gitTraining1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello World");
+	}
+
+}
