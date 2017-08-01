e5f24500bd3490ff52e0bcef91a35ee086370e5c
barceja2 <barceja2@ZHA-ITA028-W7.corp.oocl.com>
2017-08-01 08:53:32 +0800

    first commit for oxygen

---
+++ day1GitTraining/src/day1GitTraining/day1Git.java
@@ -0,0 +1,10 @@
+package day1GitTraining;
+
+public class day1Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello worldl");
+	}
+
+}
